315445204b8e1ddef4a46b11a4020d570b024128
Brian0610 <brian49345@gmail.com>
2014-07-04 19:15:33 -0400

    Changes to app.js and main.html
    Added the hadouken sound (audio element to main.html) and the code for
    it in jQuery.
    Also animated the hadouken fireball when the mouseDown event occurs.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
   $('.ryu').mouseenter(function() {
-      $('.ryu-still').hide();
-      $('.ryu-ready').show();
-
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
   })
-  .mouseleave(function(){
+  .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show(); 
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+        500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      })
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show()
+    
+  });
+});
 
-})
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}

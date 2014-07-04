6e65e4171830b3ca065e8c4e912f8030add2525e
Brian0610 <brian49345@gmail.com>
2014-07-04 17:20:31 -0400

    Streetfighter Part3
    Started adding to app.js - worked on mouseenter / mouseeleave event
    handlers for the main Ryu div

--- js/app.js
+++ js/app.js
@@ -1,34 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
   $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
+      $('.ryu-still').hide();
+      $('.ryu-ready').show();
+
   })
-  .mouseleave(function() {
+  .mouseleave(function(){
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-  .mousedown(function() {
-    playHadouken();    
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show()
-    .animate(
-      {'left': '520px'},
-      500,
-      function() {
-        $(this).hide();
-        $(this).css('left', '800px');
-      }
-    );
-  })
-  .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-  });
-});
-  
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+
+})
